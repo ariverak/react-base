90f56ce271ba2f3597890d5b1ac45eca9010036f
Juan Rivera Vargas <jrivera.v@outlook.com>
2018-01-19 00:36:09 -0300

    webpack-dev-server funcional

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,20 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
 module.exports = {
     entry: {
-        home: path.resolve(__dirname,'src/js/index.js'),
+        index: path.resolve(__dirname,'src/js/index.js'),
         contacto: path.resolve(__dirname,'src/js/contacto.js')
     },
     output: {
-        path:path.resolve(__dirname,'dist'),
-        filename: 'js/[name].js'
+        path:path.join(__dirname, 'dist'),
+        filename: 'js/[name].bundle.js'
+    },
+    devServer:{
+        open: true,
+        port: 3000,
+        contentBase: "./dist",
+        inline: true
     },
     module:{
         rules:[
@@ -16,15 +23,16 @@ module.exports = {
                 // test: que tipo de archivo quiero reconocer
                 // use: que loader se va a encargar del archivo
                 test: /\.css$/,
+                //  use: ['style-loader','css-loader']
                 use: ExtractTextPlugin.extract({
                     // ['style-loader','css-loader']
-                    // fallback: 'style-loader',
                     use: 'css-loader'
                 })
             }
         ]
     },
+    watch:true,
     plugins:[
-        new ExtractTextPlugin("css/[name].css")
+         new ExtractTextPlugin("./css/[name].css")
     ]
 }
